c0b0c7a68a8488b459280f3b858c9da9b84566d1
Daniel <dev@danielboggiano.com>
2021-12-12 11:44:13 -0500

    Implement Crypto

--- src/application/security/passwordSecurity.ts
+++ src/application/security/passwordSecurity.ts
@@ -1,15 +1,18 @@
-import bcryptjs from 'bcryptjs';
+import crypto from 'crypto';
 
-const SALT_ROUNDS = Number(process.env.SALT_ROUNDS) || 15;
+const salt = process.env.SALT!;
+const iterations = Number(process.env.ITERATIONS);
 export default class PasswordSecurity {
     constructor() { }
     async hashPassword(password: string): Promise<string> {
-        const salt = await bcryptjs.genSaltSync(SALT_ROUNDS);
-        return await bcryptjs.hashSync(password, salt);
+        const hash = await crypto.pbkdf2Sync(password, salt, iterations, 64, 'sha512');
+        return hash.toString('hex');
     }
 
     async comparePassword(password: string, hash: string): Promise<boolean> {
-        if (!password || !hash) return false;
-        return await bcryptjs.compare(password, hash);
+        if (!password || !hash)
+            return false;
+        const hashVerify = await crypto.pbkdf2Sync(password, salt, iterations, 64, 'sha512');
+        return hashVerify.toString('hex') === hash;
     }
 }
